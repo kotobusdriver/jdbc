59ddf660c9c6488668852d1334814a2352f5a6ab
kotobusdriver <utkins@gmail.com>
2025-05-17 18:03:15 +0300

    update constant error messages

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -1,4 +1,6 @@
 package org.example.app.utils;
 
 public final class Constants {
+    public final static String INCORRECT_VALUE_MSG = "Incorrect value. Try again.";
+    public static final String APP_CLOSE_MSG = "App closed.";
 }

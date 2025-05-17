c5dc06cb65b63e162325d80c9f79f1d3d0a369fb
kotobusdriver <utkins@gmail.com>
2025-05-17 18:01:03 +0300

    create skeleton project structure

---
+++ src/main/java/org/example/app/service/AppService.java
@@ -0,0 +1,11 @@
+package org.example.app.service;
+
+public class AppService {
+
+    public void handleOption(int option){
+        switch (option){
+            case 1:///// TODO
+        }
+    }
+
+}
